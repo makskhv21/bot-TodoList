cf47f84ab7e9bcb46d41e309a51cc31d9bfbcdea
repo <khvits21@gmail.com>
2024-12-13 15:34:29 +0200

    fix: Add pre-commit hook to run lint and auto-fix formatting

--- keyboards/keyboards.js
+++ keyboards/keyboards.js
@@ -1,22 +1,20 @@
 const mainMenuKeyboard = {
-    reply_markup: {
-      keyboard: [
-        ['Додати 📝', 'Переглянути 📋'], 
-        ['Запланувати ⌛', 'Команди 👨🏻‍💻'], 
-        ['Відгук ✏️', 'Вийти'],
-      ],
-      resize_keyboard: true,
-      one_time_keyboard: true,
-    },
-  };
-  
-  const addTaskKeyboard = {
-    reply_markup: {
-      keyboard: [
-        ['Завершити додавання завдань'],
-      ],
-      resize_keyboard: true,
-    },
+  reply_markup: {
+    keyboard: [
+      ['Додати 📝', 'Переглянути 📋'],
+      ['Запланувати ⌛', 'Команди 👨🏻‍💻'],
+      ['Відгук ✏️', 'Вийти'],
+    ],
+    resize_keyboard: true,
+    one_time_keyboard: true,
+  },
+};
+
+const addTaskKeyboard = {
+  reply_markup: {
+    keyboard: [['Завершити додавання завдань']],
+    resize_keyboard: true,
+  },
 };
 
 module.exports = {

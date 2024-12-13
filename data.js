40297ba43efcdf243d521cd96f2c3a0e2b6f7770
repo <khvits21@gmail.com>
2024-12-13 14:46:12 +0200

    feat: Add data for bot

---
+++ data.js
@@ -0,0 +1,10 @@
+const { token } = require('./config');
+
+const TelegramBot = require('node-telegram-bot-api');
+const userTasks = {};
+const scheduledTasks = [];
+const bot = new TelegramBot(token, { polling: true });
+
+module.exports = {
+    TelegramBot, userTasks, scheduledTasks, bot
+}

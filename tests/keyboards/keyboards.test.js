ccc5e5133773386a155d0b83b86811ad2a4383c9
repo <khvits21@gmail.com>
2024-12-14 02:03:01 +0200

    test: Add unit tests for keyboard

---
+++ tests/keyboards/keyboards.test.js
@@ -0,0 +1,30 @@
+const { mainMenuKeyboard, addTaskKeyboard } = require('../../keyboards/keyboards');
+
+describe('Keyboard configurations', () => {
+  it('should have the correct structure for mainMenuKeyboard', () => {
+    expect(mainMenuKeyboard).toHaveProperty('reply_markup');
+    expect(mainMenuKeyboard.reply_markup).toHaveProperty('keyboard');
+    expect(mainMenuKeyboard.reply_markup.keyboard).toEqual([
+      ['Додати 📝', 'Переглянути 📋'],
+      ['Запланувати ⌛', 'Команди 👨🏻‍💻'],
+      ['Відгук ✏️', 'Вийти'],
+    ]);
+  });
+
+  it('should have the correct structure for addTaskKeyboard', () => {
+    expect(addTaskKeyboard).toHaveProperty('reply_markup');
+    expect(addTaskKeyboard.reply_markup).toHaveProperty('keyboard');
+    expect(addTaskKeyboard.reply_markup.keyboard).toEqual([
+      ['Завершити додавання завдань'],
+    ]);
+  });
+
+  it('should have resize_keyboard set to true for both keyboards', () => {
+    expect(mainMenuKeyboard.reply_markup.resize_keyboard).toBe(true);
+    expect(addTaskKeyboard.reply_markup.resize_keyboard).toBe(true);
+  });
+
+  it('should have one_time_keyboard set correctly for mainMenuKeyboard', () => {
+    expect(mainMenuKeyboard.reply_markup.one_time_keyboard).toBe(true);
+  });
+});
